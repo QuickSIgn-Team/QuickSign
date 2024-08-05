fb1356a69d19f594d8597935a306dbae42000503
haxi0 <fabiomailboxgood@mail.ru>
2024-08-06 02:04:49 +0300

    Added UIAlert++, DocumentPicker (code from Lunar LOL), an actual WIP file manager

--- QuickSign/Views/ApplicationView.swift
+++ QuickSign/Views/ApplicationView.swift
@@ -1,31 +1,97 @@
-//
-//  ApplicationView.swift
-//  QuickSign
-//
-//  Created by haxi0 on 05.08.2024.
-//
-
 import SwiftUI
 
+struct DocumentsFolder: Identifiable {
+    let id = UUID()
+    let ipaName: String
+}
+
 struct ApplicationView: View {
+    @State private var selectedIpaURL: URL?
+    @State private var documentPickerDelegate: DocumentPickerDelegate?
+    @State private var ipas: [DocumentsFolder] = []
+    let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+    let fm = FileManager.default
     let sig = Signer.shared
     
     var body: some View {
         NavigationView {
             List {
                 Section {
-                    appSection()
-                    appSection()
-                    appSection()
+                    ForEach(ipas) { ipa in
+                        appStack(fileName: ipa.ipaName)
+                    }
+                } // mb a footer
+                .navigationTitle("QuickSign")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .toolbar {
+                Button(action: {
+                    importIpa()
+                }) {
+                    Image(systemName: "square.and.arrow.down")
                 }
             }
-            .navigationTitle("QuickSign")
-            .navigationBarTitleDisplayMode(.inline)
+            .refreshable {
+                await refreshFolders()
+            }
         }
-        
+        .onAppear {
+            Task {
+                await refreshFolders()
+            }
+        }
     }
     
-    private func appSection() -> some View {
+    private func refreshFolders() async {
+        do {
+            let folderContents = try fm.contentsOfDirectory(atPath: documentsPath)
+            
+            self.ipas = try await withThrowingTaskGroup(of: DocumentsFolder?.self) { group in
+                for item in folderContents {
+                    group.addTask {
+                        let itemPath = documentsPath + "/" + item
+                        
+                        guard item.hasSuffix(".ipa") else {
+                            return nil
+                        }
+                        
+                        return DocumentsFolder(ipaName: item)
+                    }
+                }
+                
+                var results: [DocumentsFolder] = []
+                for try await folder in group {
+                    if let folder = folder {
+                        results.append(folder)
+                    }
+                }
+                return results
+            }
+        } catch {
+            UIApplication.shared.alert(title: "Error Refreshing IPA!", body: "Error: \(error.localizedDescription)")
+            self.ipas = []
+        }
+    }
+    
+    private func importIpa() {
+        documentPickerDelegate = DocumentPickerDelegate { selectedURL in
+            self.selectedIpaURL = selectedURL
+            
+            do {
+                let destinationURL = URL(fileURLWithPath: documentsPath).appendingPathComponent(selectedIpaURL!.lastPathComponent)
+                try fm.copyItem(at: selectedIpaURL!, to: destinationURL)
+                
+                Task {
+                    await refreshFolders()
+                }
+            } catch {
+                UIApplication.shared.alert(title: "Error Importing IPA!", body: "Error: \(error.localizedDescription)")
+            }
+        }
+        showDocumentPicker(delegate: documentPickerDelegate!)
+    }
+    
+    private func appStack(fileName: String) -> some View {
         HStack {
             Image("DefaultIcon")
                 .resizable()
@@ -39,7 +105,7 @@ struct ApplicationView: View {
                 )
             
             VStack(alignment: .leading, spacing: 1) {
-                Text("App Placeholder")
+                Text(fileName)
                     .bold()
                     .font(.system(size: 16))
                 Text("69.0 • App Size")
