f8d27d541a14db4396567a577924529649fc4a63
haxi0 <fabiomailboxgood@mail.ru>
2024-08-06 04:26:54 +0300

    For now, the file manager is pretty much done now. Added deleting IPAs, refreshing folder, also added showing IPA file size

--- QuickSign/Views/ApplicationView.swift
+++ QuickSign/Views/ApplicationView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct DocumentsFolder: Identifiable {
     let id = UUID()
     let ipaName: String
+    let ipaSize: String
 }
 
 struct ApplicationView: View {
@@ -18,7 +19,23 @@ struct ApplicationView: View {
             List {
                 Section {
                     ForEach(ipas) { ipa in
-                        appStack(fileName: ipa.ipaName)
+                        appStack(fileName: ipa.ipaName, fileSize: ipa.ipaSize)
+                    }
+                    .onDelete { indices in
+                        for index in indices {
+                            if ipas.indices.contains(index) {
+                                let ipaToDelete = ipas[index]
+                                
+                                ipas.remove(at: index)
+                                
+                                let ipaPath = "\(documentsPath)/\(ipaToDelete.ipaName)"
+                                do {
+                                    try fm.removeItem(atPath: ipaPath)
+                                } catch {
+                                    UIApplication.shared.alert(title: "Error Deleting IPA", body: "Error: \(error.localizedDescription)")
+                                }
+                            }
+                        }
                     }
                 } // mb a footer
                 .navigationTitle("QuickSign")
@@ -26,23 +43,28 @@ struct ApplicationView: View {
             }
             .toolbar {
                 Button(action: {
+                    Task {
+                        await refreshFiles()
+                    }
+                }) {
+                    Image(systemName: "arrow.clockwise")
+                }
+                
+                Button(action: {
                     importIpa()
                 }) {
                     Image(systemName: "square.and.arrow.down")
                 }
             }
-            .refreshable {
-                await refreshFolders()
-            }
         }
         .onAppear {
             Task {
-                await refreshFolders()
+                await refreshFiles()
             }
         }
     }
     
-    private func refreshFolders() async {
+    private func refreshFiles() async {
         do {
             let folderContents = try fm.contentsOfDirectory(atPath: documentsPath)
             
@@ -50,19 +72,36 @@ struct ApplicationView: View {
                 for item in folderContents {
                     group.addTask {
                         let itemPath = documentsPath + "/" + item
+                        var fileSize: UInt64 = 0
                         
                         guard item.hasSuffix(".ipa") else {
                             return nil
                         }
                         
-                        return DocumentsFolder(ipaName: item)
+                        do {
+                            let attr = try FileManager.default.attributesOfItem(atPath: itemPath)
+                            fileSize = attr[FileAttributeKey.size] as! UInt64
+                            
+                            let dict = attr as NSDictionary
+                            fileSize = dict.fileSize()
+                        } catch {
+                            UIApplication.shared.alert(title: "Error Getting File Size of the IPA!", body: "Error: \(error.localizedDescription)")
+                            return DocumentsFolder(ipaName: "", ipaSize: "")
+                        }
+                        
+                        let byteCountFormatter = ByteCountFormatter()
+                        byteCountFormatter.allowedUnits = [.useKB, .useMB, .useGB]
+                        byteCountFormatter.countStyle = .file
+                        let formattedFileSize = byteCountFormatter.string(fromByteCount: Int64(fileSize))
+                        
+                        return DocumentsFolder(ipaName: item, ipaSize: formattedFileSize)
                     }
                 }
                 
                 var results: [DocumentsFolder] = []
-                for try await folder in group {
-                    if let folder = folder {
-                        results.append(folder)
+                for try await ipa in group {
+                    if let ipa = ipa {
+                        results.append(ipa)
                     }
                 }
                 return results
@@ -82,7 +121,7 @@ struct ApplicationView: View {
                 try fm.copyItem(at: selectedIpaURL!, to: destinationURL)
                 
                 Task {
-                    await refreshFolders()
+                    await refreshFiles()
                 }
             } catch {
                 UIApplication.shared.alert(title: "Error Importing IPA!", body: "Error: \(error.localizedDescription)")
@@ -91,7 +130,7 @@ struct ApplicationView: View {
         showDocumentPicker(delegate: documentPickerDelegate!)
     }
     
-    private func appStack(fileName: String) -> some View {
+    private func appStack(fileName: String, fileSize: String) -> some View {
         HStack {
             Image("DefaultIcon")
                 .resizable()
@@ -108,7 +147,7 @@ struct ApplicationView: View {
                 Text(fileName)
                     .bold()
                     .font(.system(size: 16))
-                Text("69.0 • App Size")
+                Text("69.0 • \(fileSize)")
                     .font(.system(size: 16))
                     .foregroundColor(.gray)
             }
