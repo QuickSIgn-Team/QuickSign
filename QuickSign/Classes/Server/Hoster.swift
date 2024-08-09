5c33e61877907db1e0eb6dece52e62d68d428154
c22dev <constclerc@gmail.com>
2024-08-09 02:51:09 +0200

    how tf did i even leave that

--- QuickSign/Classes/Server/Hoster.swift
+++ QuickSign/Classes/Server/Hoster.swift
@@ -22,24 +22,6 @@ func runServer() {
     do {
         try server.start(9090, forceIPv4: true)
         print("Server has started (port = \(try server.port())). Try to connect now...")
-        guard let url = URL(string: "http://localhost:9090/install.plist") else {
-            print("Invalid URL")
-            return
-        }
-
-        // Create and start a data task
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                print("Error: \(error)")
-                return
-            }
-            
-            if let data = data, let content = String(data: data, encoding: .utf8) {
-                print("Content:\n\(content)")
-            } else {
-                print("No data or failed to decode")
-            }
-        }.resume()
         UIApplication.shared.open(URL(string: "itms-services://?action=download-manifest&url=https://cclerc.ch/install.plist")!)// im just going to die sorry
         DispatchQueue.global().async {
             while keepServerAlive {
